3cd7b03b244a5f1679a06f907ff133faf848345b
Jenina Matela <jlsmatela@gmail.com>
2020-03-06 23:04:38 -0800

    Flashcards Lab 2

--- FlashCardLab2/ViewController.swift
+++ FlashCardLab2/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BacklabelAnswer: UILabel!
+    @IBOutlet weak var FrontlabelQuestion: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func DidTapOnFlashcard(_ sender: Any) {FrontlabelQuestion.isHidden = true
+    
+    }
+    
+  func UpdateFlashcard(question:String, answer:String) {
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardscontroller = self
+    }
+      //do Stuff Here
+
 
 }
-
