52843a585792446f2b8ab1815ee31f283d5fa2db
Jenina Matela <jlsmatela@gmail.com>
2020-03-06 19:05:07 -0800

    Initial Commit

---
+++ FlashCardLab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashCardLab2
+//
+//  Created by Jenina Matela on 3/6/20.
+//  Copyright © 2020 Jenina Matela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
